b9f8fe2d3ed82914cea8c1d39c006cb7f9557b84
nico vargas <njvnicovargas@gmail.com>
2023-11-15 16:14:33 -0300

    Agrego la primer version de Class-based views para libros

--- AppLibros/urls.py
+++ AppLibros/urls.py
@@ -15,4 +15,11 @@ urlpatterns = [
     path("socio/agregar", views.agregar_socio, name="agregar_socio"),
     path("socios/", views.listar_socios, name="socios"),
     path("index/", views.index, name="index"),
+    # URLs de CBV:
+    # se requiere que la variable que se le pase al path o la ruta se llame PK, que es la representacion de Primary Key
+    path("librosCBV/lista", views.LibroListView.as_view(), name="ListaLibros"),
+    path("librosCBV/nuevo", views.LibroCreateView.as_view(), name="NuevoLibro"),
+    path("librosCBV/<pk>", views.LibroDetailView.as_view(), name="DetalleLibro"),
+    path("librosCBV/<pk>/editar", views.LibroUpdateView.as_view(), name="EditarLibro"),
+    path("librosCBV/<pk>/borrar", views.LibroDeleteView.as_view(), name="BorrarLibro"),
 ]
--- AppLibros/views.py
+++ AppLibros/views.py
@@ -3,6 +3,14 @@ from django.template import Template, Context, loader
 from . import models
 from django.shortcuts import redirect, render
 from .forms import LibroForm, AutorForm, SocioForm
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
+# from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 
 def buscar_autor(request):
@@ -12,8 +20,19 @@ def buscar_autor(request):
 
 
 def buscar_libro(request):
+    # if request.method == "GET":
+    #     if request.GET["titulo"]:
+    #         titulo = request.GET["titulo"]
+    #         libros = models.Libro.objects.filter(titulo__icontains=titulo)
+    #         return render(
+    #             request, "buscarLibro.html", {"libros": libros, "tituloBuscado": titulo}
+    #         )
+    # else:
+    #     plantilla = loader.get_template("buscarLibro.html")
+    #     return HttpResponse(
+    #         plantilla.render({"mensaje": "No se encontró ningun libro con ese titulo"})
+    #     )
     plantilla = loader.get_template("buscarLibro.html")
-
     return HttpResponse(plantilla.render())
 
 
@@ -91,3 +110,35 @@ def agregar_autor(request):
         form = AutorForm()
 
     return render(request, "autor_form.html", {"form": form})
+
+
+class LibroListView(ListView):
+    model = models.Libro
+    context_object_name = "libros"
+    template_name = "CBV_libroLista.html"
+
+
+class LibroDetailView(DetailView):
+    model = models.Libro
+    template_name = "CBV_libroDetalle.html"
+
+
+class LibroCreateView(CreateView):
+    model = models.Libro
+    template_name = "CBV_libroCrear.html"
+    success_url = reverse_lazy("AppLibros:ListaLibros")
+    fields = ["titulo", "autor", "cantidad_paginas"]
+
+
+class LibroUpdateView(UpdateView):
+    model = models.Libro
+    template_name = "CBV_libroEditar.html"
+    success_url = reverse_lazy("AppLibros:ListaLibros")
+    fields = ["titulo", "autor", "cantidad_paginas"]
+
+
+class LibroDeleteView(DeleteView):
+    model = models.Libro
+    template_name = "CBV_libroBorrar.html"
+    success_url = reverse_lazy("AppLibros:ListaLibros")
+    fields = ["titulo", "autor", "cantidad_paginas"]
